6a20f5e21c0e755614e79ef0df55f08823a4cc02
Mercy-Muchai <mercy.muchai@andela.com>
2017-05-12 10:21:51 +0300

    set up webpack & babel config

---
+++ app/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    )
+  }
+}
+

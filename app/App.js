36e80b51e5d6419cebe7ad67d2c5fc5f0eda27e6
Mercy-Muchai <mercy.muchai@andela.com>
2017-05-12 11:20:57 +0300

    add links to new components and props.children

--- app/App.js
+++ app/App.js
@@ -7,9 +7,11 @@ export default class App extends React.Component {
     return (
       <Router history={hashHistory}>
         <Route path='/' component={Container}>
-        <IndexRoute component={Home} />
-        <Route path='/address' component={Address} />
-        <Route path='*' component={NotFound} />
+          <IndexRoute component={Home} />
+          <Route path='address' component={Address} />
+          <Route path='twitter' component={TwitterFeed} />
+          <Route path='instagram' component={Instagram} />
+          <Route path='*' component={NotFound} />
         </Route>
       </Router>
     )
@@ -17,10 +19,21 @@ export default class App extends React.Component {
 }
 
 const Home = () => <h1> Hello World! </h1>
-const Address = () => <h1> We are located at Kindaruma Road </h1>
+const Address = (props) => <div>
+  <br />
+  <Link to='/address'>Twitter Feed</Link>&nbsp;
+  <Link to='/address/instagram'>Instagram Feed</Link>
+  <h1> We are located at Kindaruma Road </h1>
+  <h3> Twitter Feed </h3>
+  
+  {props.children}
+</div>
 const NotFound = () => <h1> This page is not found! </h1>
 const Nav = () => <div>
   <Link to='/'>Home</Link>&nbsp;
   <Link to='/address'>Address</Link>
-  </div>
+</div>
 const Container = (props) => <div> <Nav /> {props.children} </div>
+const Instagram = () => <h3> Instagram Feed </h3>
+const TwitterFeed = () => <h3> Twitter Feed </h3>
+

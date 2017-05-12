df0bb34093aebef8bd23f4a5b5ac299845203fa1
Mercy-Muchai <mercy.muchai@andela.com>
2017-05-12 10:57:51 +0300

    Link routes

--- app/App.js
+++ app/App.js
@@ -6,9 +6,11 @@ export default class App extends React.Component {
   render() {
     return (
       <Router history={hashHistory}>
-        <Route path='/' component={Home} />
+        <Route path='/' component={Container}>
+        <IndexRoute component={Home} />
         <Route path='/address' component={Address} />
         <Route path='*' component={NotFound} />
+        </Route>
       </Router>
     )
   }
@@ -17,3 +19,8 @@ export default class App extends React.Component {
 const Home = () => <h1> Hello World! </h1>
 const Address = () => <h1> We are located at Kindaruma Road </h1>
 const NotFound = () => <h1> This page is not found! </h1>
+const Nav = () => <div>
+  <Link to='/'>Home</Link>&nbsp;
+  <Link to='/address'>Address</Link>
+  </div>
+const Container = (props) => <div> <Nav /> {props.children} </div>
